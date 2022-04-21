87c268c7be07d4395788bbe588203713d0e027c3
Marat Elagin <maratelagin@gmail.com>
2022-04-21 13:29:53 +0300

    Countable Bloom Filter

---
+++ CBF/CBF/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using CBF;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        string text = "";
+        var dir = new DirectoryInfo(Directory.GetCurrentDirectory()).Parent.Parent.Parent.ToString();
+        var filePath = Path.Combine(dir, "text.txt");
+        
+        using (var sr = new StreamReader(filePath))
+            text = sr.ReadToEnd();
+        
+        char[] separators = {' ', '\n','\r', '-', ',', '.', ':', ';', '?', '!', ')', '('};
+        string[] words = text
+            .Split(separators,
+                StringSplitOptions.RemoveEmptyEntries)
+            .Distinct()
+            .ToArray();
+
+        var cbf = new Cbf(words.Length, 0.1);
+        foreach (var word in words)
+            cbf.Add(word);
+
+        Console.WriteLine(cbf.FindString("улыбки"));
+        
+        var filterFilePath = Path.Combine(dir, "filter.txt");
+        cbf.WriteFilterToFile(filterFilePath);
+    }
+}
